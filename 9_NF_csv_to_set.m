476b5a5b8faf9ad76e476351f0c7c584afb7fc03
Osama Ejaz <59282708+osamaejz@users.noreply.github.com>
2023-06-14 16:34:12 +0500

    Update 9_NF_csv_to_set.m

--- 9_NF_csv_to_set.m
+++ 9_NF_csv_to_set.m
@@ -1,3 +1,4 @@
+clear all
 read_folder = 'C:\Users\Dell\Desktop\NF Data_CSV\';
 Files_dir =dir(read_folder);
 save_location = 'C:\Users\Dell\Desktop\NF Data_SET';
@@ -6,13 +7,11 @@ eeglab
 load NF_loc_22.mat
 close all
 
-
-matdata = {};
 for k = 1:(length(Files_dir)-2)
     filename = strcat(read_folder, Files_dir(k+2).name);
     data = csvread(filename,1,1);
     data = data(:,2:23);
-    EEG.data = data;
+    EEG.data = data';
     EEG.nbchan = 22;
 
     EEG = pop_chanedit(EEG , 'lookup' , 'E:\\eeglab_current\\eeglab2020_0\\plugins\\dipfit\\standard_BESA\\standard-10-5-cap385.elp' , 'load',{'E:\eeglab_current\eeglab2020_0\NF_Cap22.ced' , 'filetype' , 'autodetect'});
